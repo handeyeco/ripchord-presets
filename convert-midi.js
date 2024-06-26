96ed95c40e0b71eb523629c3fc73df29047c4330
handeyeco <matthew.c.tx@gmail.com>
2024-06-26 07:48:56 -0500

    options

--- convert-midi.js
+++ convert-midi.js
@@ -1,5 +1,37 @@
-const { program } = require('commander');
+const fs = require("fs")
+const { program } = require("commander");
+const midiParser  = require('midi-parser-js');
 
 program
-  .option('-i, --input <path>', 'input path', './input')
-  .option('-o, --output <path>', 'output path', "./output");
+  .option("-i, --input <path>", "input path", "./input")
+  .option("-o, --output <path>", "output path", "./output")
+  .option("-s --single", "include single chord files", false);
+
+program.parse();
+const options = program.opts();
+
+function convertFile(path) {
+  console.log("file: " + path)
+}
+
+function convertDir(path) {
+  console.log("dir: " + path)
+}
+
+function main() {
+  if (!fs.existsSync(options.input)) {
+    console.error('input path not found')
+  }
+
+  if (!fs.existsSync(options.output)) {
+    console.error('output path not found')
+  }
+
+  if (fs.lstatSync(options.input).isDirectory()) {
+    convertDir(options.input)
+  } else {
+    convertFile(options.input)
+  }
+}
+
+main()
